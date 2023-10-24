b16a4c283aeb119e3b1b146b021c7b323be36179
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-10-25 04:33:43 +0530

    1. feat: implement base funcs to scan source input

--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/chettriyuvraj/lox-crafting-interpreters/pkg/scanner"
+	"io"
+	"os"
+	// "github.com/chettriyuvraj/lox-crafting-interpreters/pkg/scanner"
 )
 
+var start, current, line int = 0, 0, 0
+
 func main() {
-	fmt.Println("Hi")
-	scanner.ScanTokens()
+	// scanner.ScanTokens()
+	if len(os.Args) < 2 {
+		runPrompt()
+	} else if len(os.Args) == 2 {
+		err := runFile(os.Args[1])
+		fmt.Println(err)
+	} else {
+		fmt.Println("Usage: go run main.go [file]")
+	}
+}
+
+func runPrompt() error {
+	fmt.Println("Run prompt!")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				run(s)
+				return nil
+			}
+			return err
+		}
+		run(s)
+	}
+
+	return nil
+}
+
+func runFile(filePath string) error {
+	fmt.Println("Run file!")
+
+	_, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func run(source string) {
+	fmt.Println("running run!")
 }
